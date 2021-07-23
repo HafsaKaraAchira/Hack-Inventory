8f1d4ee14c537269213c0ccd77bb7fb6ff49a16e
Ariful Haque <ArifulH6464@gmail.com>
2021-07-23 20:23:35 +0100

    basic equipment form

--- hack_inventory/lib/equipmentRequest.dart
+++ hack_inventory/lib/equipmentRequest.dart
@@ -13,7 +13,36 @@ class _EquipRequest extends State<EquipRequest> {
         title: Text('Equipment Request'),
       ),
       body: Center(
-        child: Text('Request Below'),
+        // child: Text('Request Below'),
+        child: Column(
+          children: [
+            Text('Request Below'),
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter The Equipment You Want',
+                      labelText: 'Equipment',
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter Your Team Number',
+                      labelText: 'Team Number',
+                    ),
+                  ),
+                  new Container(
+                    child: new ElevatedButton(
+                      child: const Text('Submit'),
+                      onPressed: null,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
