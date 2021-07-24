5310380698c985f948ac533cd8d4887c077ed8ee
NaveenGokavarapu19 <66323587+NaveenGokavarapu19@users.noreply.github.com>
2021-07-24 23:48:53 +0530

    Create fire_base_stub.dart

---
+++ hack_inventory/lib/Views/fire_base_stub.dart
@@ -0,0 +1,8 @@
+import "package:flutter/material.dart";
+
+class validation extends StatelessWidget {
+  final VoidCallback selectHandler;
+
+  // Firebase function should return valid or not 
+  
+  Answer(this.selectHandler)
