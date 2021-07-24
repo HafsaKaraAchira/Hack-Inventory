cff8690afb07659303e44d2951d14429abceabf0
Ariful Haque <ArifulH6464@gmail.com>
2021-07-24 12:31:14 +0100

    Still Broken - Missing Concrete

---
+++ hack_inventory/lib/Views/HackathonGoerCode.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class HackathonGoerCode extends StatelessWidget {
+  @override
+  _HackathonGoerCode createState() => _HackathonGoerCode();
+}
+
+class _HackathonGoerCode extends State<HackathonGoerCode> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Hey there'),
+      ),
+    );
+  }
+}
